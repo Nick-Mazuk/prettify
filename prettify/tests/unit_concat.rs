fa66579b6c77a5587ea6ca7183050bd7e085b623
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2022-06-17 22:43:53 -0700

    reorganize tests

--- prettify/tests/unit_concat.rs
+++ prettify/tests/unit_concat.rs
@@ -1,19 +1,6 @@
 use prettify::{concat, print, string};
 
 #[test]
-fn doc_string_1() {
-    assert_eq!(print(string("hello world")), "hello world".to_string());
-}
-
-#[test]
-fn doc_string_2() {
-    assert_eq!(
-        print(string("another string")),
-        "another string".to_string()
-    );
-}
-
-#[test]
 fn doc_children_string() {
     assert_eq!(
         print(concat(vec!(
---
+++ prettify/tests/unit_string.rs
@@ -0,0 +1,14 @@
+use prettify::{concat, print, string};
+
+#[test]
+fn doc_string_1() {
+    assert_eq!(print(string("hello world")), "hello world".to_string());
+}
+
+#[test]
+fn doc_string_2() {
+    assert_eq!(
+        print(string("another string")),
+        "another string".to_string()
+    );
+}
