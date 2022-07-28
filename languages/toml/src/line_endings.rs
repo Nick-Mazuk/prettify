a6141acc3c764dd82ef1a92dd539e1b8c3b2af10
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2022-07-28 16:53:28 -0700

    parse toml blank lines

--- languages/toml/src/line_endings.rs
+++ languages/toml/src/line_endings.rs
@@ -35,6 +35,22 @@ pub fn line_end_with_optional_comment(input: &str) -> nom::IResult<&str, Prettif
     ))
 }
 
+pub fn blank_line_with_optional_comment(input: &str) -> nom::IResult<&str, PrettifyDoc> {
+    let (remainder, (_, comment, _)) =
+        tuple((space0, opt(comment), alt((line_ending, eof))))(input)?;
+
+    Ok((
+        remainder,
+        concat(vec![
+            match comment {
+                Some(comment) => comment,
+                None => string(""),
+            },
+            hard_line(),
+        ]),
+    ))
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -61,5 +77,31 @@ mod test {
         assert_formatted(line_end_with_optional_comment("#\n\n"), ("\n", "\n"));
         assert_formatted(line_end_with_optional_comment("\n\n"), ("\n", "\n"));
         assert_formatted(line_end_with_optional_comment("        \n\n"), ("\n", "\n"));
+    }
+
+    #[test]
+    fn blank_line_with_optional_comment_test() {
+        assert_formatted(
+            blank_line_with_optional_comment("# comment"),
+            ("", "# comment\n"),
+        );
+        assert_formatted(
+            blank_line_with_optional_comment("#comment    "),
+            ("", "# comment\n"),
+        );
+        assert_formatted(
+            blank_line_with_optional_comment("   # comment  \n"),
+            ("", "# comment\n"),
+        );
+        assert_formatted(
+            blank_line_with_optional_comment("# comment\n\n"),
+            ("\n", "# comment\n"),
+        );
+        assert_formatted(blank_line_with_optional_comment("#\n\n"), ("\n", "\n"));
+        assert_formatted(blank_line_with_optional_comment("\n\n"), ("\n", "\n"));
+        assert_formatted(
+            blank_line_with_optional_comment("        \n\n"),
+            ("\n", "\n"),
+        );
     }
 }
